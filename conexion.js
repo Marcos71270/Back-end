8285e7bc520bc4c090540ca1e21db054ab085b96
Tu Nombre <marcogonzo71270@gmail.com>
2024-06-19 21:22:32 -0500

    Se añaden variables de entorno

--- conexion.js
+++ conexion.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2/promise');
 // Create the connection to database
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'login2',
+    host: process.env.HOSTDB ||'localhost',
+    user: process.env.USERDB ||'root',
+    database: process.env.DB || 'login2',
+    password: process.env.PASWORDDB ||'',
+    port: process.env.PORTDB || 3306,
 });
 
 module.exports = connection;
