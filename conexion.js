7d2faf25e99aa44ccb9e3421d30b31a667a68d90
Tu Nombre <marcogonzo71270@gmail.com>
2024-06-22 21:54:52 -0500

    Espacios

--- conexion.js
+++ conexion.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 // Create the connection to database
 const connection = mysql.createPool({
-    host: process.env.HOSTDB ||'localhost',
-    user: process.env.USERDB ||'root',
+    host: process.env.HOSTDB || 'localhost',
+    user: process.env.USERDB || 'root',
     database: process.env.DB || 'login2',
-    password: process.env.PASWORDDB ||'',
+    password: process.env.PASWORDDB || '',
     port: process.env.PORTDB || 3306,
 });
 
